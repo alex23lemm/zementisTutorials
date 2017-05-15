6eb10e7adaf1c23ac476eb71e292b1fd4cebbe57
Alex Lemm <alex23lemm@gmail.com>
2017-05-15 22:47:17 +0200

    Migrate to learnr package

--- setup.R
+++ setup.R
@@ -1,9 +1,6 @@
 # Install packages used in the exercises
 install.packages(c("httr", "ggplot2", "purrr", "dplyr", "tidyr", 
                    "randomForest", "rpart", "rpart.plot", "caret", "XML", "pmml",
-                   "jsonlite"))
+                   "jsonlite", "learnr"))
 
-# Install tutor package from GitHub and necessary dependencies
-install.packages("devtools")
-install.packages(c("backports", "httpuv", "xtable"))
-devtools::install_github("rstudio/rtutor")
+
