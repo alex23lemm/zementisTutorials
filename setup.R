7753eea3ed2d60ee81a5041d6b5cb493e667dfb3
Alex Lemm <alex23lemm@gmail.com>
2017-07-04 22:55:37 +0200

    Delete setup.R

--- setup.R
+++
@@ -1,6 +0,0 @@
-# Install packages used in the exercises
-install.packages(c("httr", "ggplot2", "purrr", "dplyr", "tidyr", 
-                   "randomForest", "rpart", "rpart.plot", "caret", "XML", "pmml",
-                   "jsonlite", "learnr"))
-
-
